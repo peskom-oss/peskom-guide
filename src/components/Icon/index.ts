28021bb724efb5ffeef0aedcb126b538a27f1b19
cantdocpp <cihuyman95@gmail.com>
2025-11-20 16:08:53 +0700

    add github logo

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,3 +1,4 @@
 export { default as FacebookIcon } from "./FacebookIcon.astro";
 export { default as InfoIcon } from "./InfoIcon.astro";
 export { default as DangerIcon } from "./DangerIcon.astro";
+export { default as GithubIcon } from "./GithubIcon.astro";
